972c18fad9de0db544c02dabb1af3caef8ca9e7a
EJ <ejg132@gmail.com>
2019-10-11 12:41:05 -0700

    all test passing and implemented jwt password verification

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,13 @@
 process.env.TZ = 'UTC'
 process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
 
 require('dotenv').config()
 const { expect } = require('chai')
 const supertest = require('supertest')
+
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://dunder_mifflin@localhost/thingful-auth-test"
 
 global.expect = expect
 global.supertest = supertest
